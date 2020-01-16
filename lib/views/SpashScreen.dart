f9ac205a64012e6d9813ffb64d84e167cd1faeb1
Alejandro Demetrio <alejandro.demetrio13@gmail.com>
2020-01-16 20:39:47 +0000

    project configured for Firebase

---
+++ lib/views/SpashScreen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SpashScreenView extends StatefulWidget {
+
+  @override
+  _SplashScreenView createState() => _SplashScreenView();
+}
+
+class _SplashScreenView extends State<SpashScreenView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(53, 50, 69, 1),
+      body: Center(
+
+      ),
+    );
+  }
+
+}
